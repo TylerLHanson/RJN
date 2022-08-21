4cd93f0eb88ab3597395852c8730666ace1ae36a
Tyler <TylerLHanson@gmail.com>
2022-08-21 09:18:57 -0700

    change random selections

--- rjn.py
+++ rjn.py
@@ -5,8 +5,6 @@ class RecJet:
     def __init__(self):
         self.recipe_list = []
         self.filtered_list = []
-        self.filtered_dinners = []
-        self.filtered_smoothies = []
 
     def insert_single_recipe(self, category, name, ingredients):
         # needs errors and exception handling
@@ -30,34 +28,51 @@ class RecJet:
             print(x[1])
 
     def random_selection_smoothies(self, num):
-        self.filtered_smoothies = [x for x in self.recipe_list if x[0] == "smoothie"]
-        self.filtered_smoothies = random.sample(self.filtered_smoothies, num)
+        self.smoothies = [x for x in self.recipe_list if x[0] == "smoothie"]
+        self.selected_smoothies = random.sample(self.smoothies, num)
         # TODO add option parameter
-        self.filtered_list.extend(self.filtered_smoothies)
-        for x in self.filtered_smoothies:
+        if not self.filtered_list:
+            self.tmp = [x for x in self.recipe_list if x[0] != "smoothie"]
+        else:
+            self.tmp = [x for x in self.filtered_list if x[0] != "smoothie"]
+        self.tmp.extend(self.selected_smoothies)
+        self.filtered_list = self.tmp
+        for x in self.selected_smoothies:
             print(x[1])
 
     def random_selection_dinners(self, num):
-        self.filtered_dinners = [x for x in self.recipe_list if x[0] == "dinner"]
-        self.filtered_dinners = random.sample(self.filtered_dinners, num)
+        self.dinners = [x for x in self.recipe_list if x[0] == "dinner"]
+        self.selected_dinners = random.sample(self.dinners, num)
         # TODO add option parameter
-        self.filtered_list.extend(self.filtered_dinners)
-        for x in self.filtered_dinners:
+        if not self.filtered_list:
+            self.tmp = [x for x in self.recipe_list if x[0] != "dinner"]
+        else:
+            self.tmp = [x for x in self.filtered_list if x[0] != "dinner"]
+        self.tmp.extend(self.selected_dinners)
+        self.filtered_list = self.tmp
+        for x in self.selected_dinners:
             print(x[1])
 
     def specific_selection_any(self, *args):
         for x in self.recipe_list:
             if x[1] in args:
-                self.filtered_dinners.append(x)
-        self.filtered_list.extend(self.filtered_dinners)
+                self.filtered_list.append(x)
 
     def generate_shopping_list(self):
         self.aggregated_list = {}
-        for x in self.recipe_list:
-            for a,b in x[2].items():
-                if a not in self.aggregated_list:
-                    self.aggregated_list[a] = b
-                else:
-                    self.aggregated_list[a] += b
+        if not self.filtered_list:
+            for x in self.recipe_list:
+                for a,b in x[2].items():
+                    if a not in self.aggregated_list:
+                        self.aggregated_list[a] = b
+                    else:
+                        self.aggregated_list[a] += b
+        else:
+            for x in self.filtered_list:
+                for a,b in x[2].items():
+                    if a not in self.aggregated_list:
+                        self.aggregated_list[a] = b
+                    else:
+                        self.aggregated_list[a] += b
                     
         return self.aggregated_list
