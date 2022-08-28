0523fd8fc2386cb6a7eedbaef25a67acca706745
Tyler <TylerLHanson@gmail.com>
2022-08-28 08:58:25 -0700

    test connection

--- qa.py
+++ qa.py
@@ -1,2 +1,15 @@
 import ccxt
 print(ccxt.exchanges)
+
+kucoin = ccxt.kucoin({
+    'apikey': 'xxxx'
+})
+
+print(kucoin)
+
+symbol = 'BTC-USDT'
+timeframe = '15m'
+limit = 500
+
+bars = kucoin.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
+print(bars)
