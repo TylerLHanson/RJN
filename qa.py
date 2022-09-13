684fa540153ebec01a9d551188d9f754bdb9655c
Tyler <TylerLHanson@gmail.com>
2022-09-13 09:54:15 -0700

    del book

--- qa.py
+++ qa.py
@@ -11,10 +11,14 @@ import requests
 #     'password': local_config.KUCOIN_PASSPHRASE,
 # })
 
-# markets = exchange.load_markets()
+exchange = ccxt.coinbaseprime()
 
-# for x in markets:
-#     print(x)
+markets = exchange.load_markets()
+
+for x in markets:
+    print(x)
+# https://exchange.coinbase.com/markets
+
 
 # Horizen
 # ticker = exchange.fetch_ticker('ZEN/USDT')
@@ -34,39 +38,9 @@ import requests
 # 19:00
 
 
-c = requests.get("https://api.opensea.io/api/v1/collections")
-print(c.json())
+# c = requests.get("https://api.opensea.io/api/v1/collections")
+# print(c.json())
 
-b = requests.get("https://api.opensea.io/api/v1/collections")
-print(b.json())
-
-
-# Add parent hash
-class Block:
-    id = None
-    history = None
-    parent_id = None
-
-block_A = Block()
-block_A.id = 1
-block_A.history = 'Nelson likes cat'
-
-block_B = Block()
-block_B.id = 2
-block_B.history = 'Marie likes dog'
-block_B.parent_id = block_A.id
-
-block_C = Block()
-block_C.id = 3
-block_C.history = 'Sky hates dog'
-block_C.parent_id = block_B.id
-
-
-
-
-
-
-
-
-
+# b = requests.get("https://api.opensea.io/api/v1/collections")
+# print(b.json())
 
